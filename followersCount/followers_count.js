68eab0713805b92462c22ef4858c944159b4526f
Moath Hazeem <your_email@example.com>
2025-12-07 18:54:52 +0200

    new edit file

---
+++ followersCount/followers_count.js
@@ -0,0 +1,24 @@
+let count = 0;
+function increaseCount(){
+count++;
+}
+function displayCount(){
+    document.getElementById('countDisplay').innerHTML=count
+}
+function increaseCount(){
+    count++;
+    displayCount();
+}
+function checkCountValue(){
+    if(count === 10){
+        alert("Your Instagram post has gained 10 followers! Congratulations!")
+    }
+    else if (count === 20) {
+        alert("Your Instagram post has gained 20 followers! Keep it up!")
+    }
+}
+function increaseCount(){
+    count++;
+    displayCount();
+    checkCountValue();
+}
